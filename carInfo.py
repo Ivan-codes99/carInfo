e26c7d602207fff80cd2521df24f2ca58d40f431
Ivan Figueroa <112541591+ifigu029@users.noreply.github.com>
2023-06-28 00:38:23 -0400

    Initializing values

--- carInfo.py
+++ carInfo.py
@@ -1,1 +1,16 @@
-#Reddit bot that gives information on a specific car-
+#Reddit bot that gives information on a specific car
+import praw
+
+#initializing
+client_id = ""
+client_secret = ""
+username = ""
+password = ""
+user_agent = ""
+
+#creating reddit instance
+reddit = praw.reddit(client_id = client_id,
+                     client_secret = client_secret,
+                     username = username,
+                     password = password,
+                     user_agent = user_agent)
