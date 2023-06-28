75e9965c2df15ffd02a992ec39e7c1555b7a003a
Ivan Figueroa <112541591+ifigu029@users.noreply.github.com>
2023-06-28 02:21:18 -0400

    Added defs
    Added the login and run_bot methods

--- carInfo.py
+++ carInfo.py
@@ -8,9 +8,29 @@ username = ""
 password = ""
 user_agent = ""
 
+#Creating a set of car models, short set for testing purposes right now
+car_models = {"camry", "corolla", "civic", "brz", "86", "supra", "m3", "accord"}
+
 #creating reddit instance
-reddit = praw.reddit(client_id = client_id,
-                     client_secret = client_secret,
-                     username = username,
-                     password = password,
-                     user_agent = user_agent)
+def login():
+    reddit = praw.Reddit(client_id = client_id,
+                        client_secret = client_secret,
+                        username = username,
+                        password = password,
+                        user_agent = user_agent)
+    return reddit
+
+def run_bot(r):
+    subreddit = r.subreddit('test')  # Retrieve the subreddit object
+    for comment in subreddit.stream.comments():
+        for item in car_models:
+            if item in comment.body:
+                print("Car model: " + item + " found in comment.")
+
+reddit = login()
+run_bot(reddit)
+
+
+
+
+
