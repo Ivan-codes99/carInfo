0c30d8994e3ae12dc55820a2de1b7faeac8ec182
Ivan Figueroa <Ivanfiggomez@gmail.com>
2023-07-24 01:27:27 -0400

    Added 3 more functions to fetch body type, exterior colors, and interior colors
    Added 3 more functions to fetch body type, exterior colors, and interior colors.
    Additionally, if the model is "is", it will only recognize at as IS, to prevent confusion with the very common english word "is"

--- carInfo.py
+++ carInfo.py
@@ -22,10 +22,22 @@ def run_bot(r, comments_replied_to):
         # print(lst_str_comment)
         for word in lst_str_comment:
             if word in [model.lower() for model in car_models] and (comment.id not in comments_replied_to) and (comment.author != r.user.me()): #conditionals check for comment author and previously replied to comments
+                
                 model_trims = fetchTrims(word)
+                if word == "is":
+                    word = word.upper() #the model "is" clearly can be confused with the very common english word "is" therefore this model will only be recognized by the bot as IS.
                 str_model_trims = ', '.join(str(element) for element in model_trims)
+                model_ext_colors = fetchExColors(word)
+                str_model_ext_colors = ', '.join(str(element) for element in model_ext_colors)
+                model_int_colors = fetchInColors(word)
+                str_model_int_colors = ", ".join(str(element) for element in model_int_colors)
+                model_body_types = fetchBodyTypes(word)
+                str_model_body_types = ", ".join(str(element) for element in model_body_types)
+                
                 print("Beep boop: Car model: " + word + " found in comment." + "\n" + "Availabe trims: " + str_model_trims)
-                comment.reply("Beep boop: Car model: " + word + " found in comment." + "\n" + "Availabe trims: " + str_model_trims)
+                comment.reply("Beep boop: Car model: " +"\"" + word + "\"" + " found in comment." + "\n\n" + "Availabe trims: " + str_model_trims + ".\n\n Available exterior colors: " + str_model_ext_colors + "." +
+                              "\n\n Available interior colors: " + str_model_int_colors + ".\n\n" +
+                              "Available body types: " + str_model_body_types + ".")
                 comments_replied_to.append(comment.id)
 
                 with open("savedComments.txt", "a") as pc:
@@ -57,9 +69,8 @@ def Authorize():
         print("Authentication failed. Status code:", response.status_code)
 
 
-def fetchCarModels():
+def fetchCarModels(year = "year=2020"):
     api_url = "https://carapi.app/api/models?year=2020"
-    year = "year=2020"
     currpage = 1
     headers = {
         'accept': 'application/json',
@@ -82,6 +93,7 @@ def fetchCarModels():
         else:
             print("API request failed. Status code:", response.status_code)
             break
+        
 
 def fetchTrims(model, year="year=2020"):
     model = model.capitalize()
@@ -95,7 +107,7 @@ def fetchTrims(model, year="year=2020"):
     search_params = {
         "year": 2020
     }
-    
+    currpage = 1
     while api_url:
         response = requests.get(api_url, params = search_params, headers=headers)
         if response.status_code == 200:
@@ -103,10 +115,100 @@ def fetchTrims(model, year="year=2020"):
             for item in data["data"]:
                 set_trims.add(item["name"])
             break
+        if "next" in data["collection"] != "":
+            currpage +=1
+            api_url = "https://carapi.app/api/trims?page=" + str(currpage) + "&" + year  + "&model=" + model
         else:
             print("API request failed. Status code:", response.status_code)
             break
     return set_trims
+
+def fetchExColors(model, year="year=2020"):
+    set_exterior_colors = set()
+    api_url = "https://carapi.app/api/exterior-colors?year=2020&model=" + model
+    headers = {
+        'accept': 'application/json',
+        'Authorization' : f'Bearer {jwt_token}'
+    }
+
+    search_params = {
+        "year": 2020
+    }
+    currpage = 1
+    while api_url:
+        response = requests.get(api_url, params = search_params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data["data"]:
+                set_exterior_colors.add(item["name"])
+            if "next" in data["collection"] != "":
+                currpage +=1
+                api_url = "https://carapi.app/api/exterior-colors?page=" + str(currpage) + "&" + year  + "&model=" + model
+            else:
+                api_url = None
+        else:
+            print("API request failed. Status code:", response.status_code)
+            break
+    return set_exterior_colors
+
+def fetchInColors(model, year="year=2020"):
+    set_interior_colors = set()
+    api_url = "https://carapi.app/api/interior-colors?year=2020&model=" + model
+    headers = {
+        'accept': 'application/json',
+        'Authorization' : f'Bearer {jwt_token}'
+    }
+
+    search_params = {
+        "year": 2020
+    }
+    currpage = 1
+    while api_url:
+        response = requests.get(api_url, params = search_params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data["data"]:
+                set_interior_colors.add(item["name"])
+            if "next" in data["collection"] != "":
+                currpage +=1
+                api_url = "https://carapi.app/api/exterior-colors?page=" + str(currpage) + "&" + year  + "&model=" + model
+            else:
+                api_url = None
+                break
+        else:
+            print("API request failed. Status code:", response.status_code)
+            break
+    return set_interior_colors
+
+def fetchBodyTypes(model, year = "2020"):
+    set_body_types = set()
+    api_url = "https://carapi.app/api/bodies?year=2020&model=" + model
+    headers = {
+        'accept': 'application/json',
+        'Authorization' : f'Bearer {jwt_token}'
+    }
+
+    search_params = {
+        "year": 2020
+    }
+    currpage = 1
+    while api_url:
+        response = requests.get(api_url, params = search_params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data["data"]:
+                set_body_types.add(item["type"])
+            if "next" in data["collection"] != "":
+                currpage +=1
+                api_url = "https://carapi.app/api/bodies?page=" + str(currpage) + "&" + year  + "&model=" + model
+            else:
+                api_url = None
+                break
+        else:
+            print("API request failed. Status code:", response.status_code)
+            break
+    return set_body_types
+    
     
 #initializing
 usrname = ""
